4f1b2016f9c4071d649d3a1c101b833218e6c430
Ståle Pettersen <staale@gmail.com>
2012-11-27 23:08:39 +0100

    Added validation demo. Added headings in templates for other demos

---
+++ js/05-ValidationCtrl.js
@@ -0,0 +1,18 @@
+// Based on http://docs.angularjs.org/guide/forms
+function ValidationCtrl($scope) {
+    $scope.master= {};
+
+    $scope.update = function(user) {
+        $scope.master= angular.copy(user);
+    };
+
+    $scope.reset = function() {
+        $scope.user = angular.copy($scope.master);
+    };
+
+    $scope.isUnchanged = function(user) {
+        return angular.equals(user, $scope.master);
+    };
+
+    $scope.reset();
+}
